b372660ceccffa9b3e7396e5661679b79d8933de
lawrinda <lawrinda.obo@amalitechtraining.org>
2024-09-26 16:14:32 +0000

    Modified the program for easy reading

--- cal_factorial.c
+++ cal_factorial.c
@@ -10,7 +10,7 @@ printf("Enter number: ");
 scanf("%d", &number);
 
 result = sum(number);
-printf("sum = %d", result);
+printf("The factorial of %d is = %d",number, result);
   return 0;
 }
 int sum(int n){
