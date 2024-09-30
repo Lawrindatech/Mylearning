e362c63da9ee180f909e1bc6b7a694a51565e404
lawrinda <lawrinda.obo@amalitechtraining.org>
2024-09-30 14:51:50 +0000

    copied text from students_mrks.txt to newText.txt

--- tempCodeRunnerFile.c
+++ tempCodeRunnerFile.c
@@ -1,30 +1,43 @@
-// C program to sort array elements in descending order 
-#include <stdio.h> 
+#include <stdio.h>
+#include <stdlib.h> // For exit()
 
-int main() 
-{ 
+int main()
+{
+    FILE *fptr1, *fptr2;
+    char filename[100];
+    int c;
 
-	int a[5] = { 4, 20, 100, 66, 37 }; 
-	int n = 5, i, j, t = 0; 
-	
-	// iterates the array elements 
-	for (i = 0; i < n; i++) { 
-		
-		// iterates the array elements from index 1 
-		for (j = i + 1; j < n; j++) { 
-			
-			// comparing the array elements, to set array 
-			// elements in descending order 
-			if (a[i] < a[j]) { 
-				t = a[i]; 
-				a[i] = a[j]; 
-				a[j] = t; 
-			} 
-		} 
-	} 
-	// printing the output 
-	for (i = 0; i < n; i++) { 
-		printf("%d ", a[i]); 
-	} 
-	return 0; 
+    printf("Enter the filename to open for reading: ");
+    scanf("%s", filename);
+
+    // Open one file for reading
+    fptr1 = fopen(filename, "r");
+    if (fptr1 == NULL)
+    {
+        printf("Cannot open file %s\n", filename);
+        exit(1);
+    }
+
+    printf("Enter the filename to open for writing: ");
+    scanf("%s", filename);
+
+    // Open another file for writing
+    fptr2 = fopen(filename, "w");
+    if (fptr2 == NULL)
+    {
+        printf("Cannot open file %s\n", filename);
+        exit(1);
+    }
+
+    // Read contents from file
+    while ((c = fgetc(fptr1)) != EOF)
+    {
+        fputc(c, fptr2);
+    }
+
+    printf("Contents copied to %s\n", filename);
+
+    fclose(fptr1);
+    fclose(fptr2);
+    return 0;
 }
